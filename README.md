aa596a0270e6356539867d5afb6e9cd15fb5108b
Gihara Wicktramatunge <giharanw@gmail.com>
2022-02-25 13:04:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# MySQL Client Shell Application
+
+### LICENSE
+Copyright © 2022 DEP8. All Rights Reserved.
+This project is licensed under [MIT](LICENSE.txt) license.
---
+++ src/controller/LoginFormController.java
@@ -0,0 +1,38 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginFormController {
+
+    
+    public TextField txtHost;
+
+    
+    public TextField txtPort;
+
+    
+    public TextField txtUsername;
+
+    
+    public PasswordField txtPassword;
+
+    
+    public Button btnConnect;
+
+    
+    public Button btnExit;
+
+    
+    public void btnConnectOnAction(ActionEvent event) {
+
+    }
+
+    
+    public void btnExitOnAction(ActionEvent event) {
+
+    }
+
+}

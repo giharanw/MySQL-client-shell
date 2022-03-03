2099e5da1df08b29eddc36eb970e612f7783b03f
Gihara Wicktramatunge <giharanw@gmail.com>
2022-03-03 10:09:13 +0530

    Destroy mysql instance when the client is closed

--- src/controller/ShellFormController.java
+++ src/controller/ShellFormController.java
@@ -1,21 +1,39 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+
+import java.io.IOException;
 
 public class ShellFormController {
     public TextArea txtCommand;
     public TextArea txtOutput;
     public Button btnExecute;
-    private String host;
-    private String port;
-    private String userName;
-    private String password;
+    private Process mysql;
 
     public void btnExecuteOnAction(ActionEvent actionEvent) {
     }
 
     public void initdata(String host, String port, String userName, String password) {
+        try{
+            mysql = new ProcessBuilder("mysql","-h",host,"--port",port,"-u",userName,"-p").start();
+            mysql.getOutputStream().write(password.getBytes());
+            mysql.getOutputStream().flush();
+
+            txtCommand.getScene().getWindow().setOnCloseRequest(event -> {
+                if (mysql.isAlive()){
+                    mysql.destroy();
+                }
+            });
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, "Failed to establish the connection for some reason").show();
+            if (mysql.isAlive()){
+                mysql.destroyForcibly();
+            }
+        }
     }
 }
